37118490fd635a87646bca2ddf3e6e4b97764f18
Tad Thorley <phaedryx@gmail.com>
2014-01-09 22:23:44 -0700

    take out all of the other javascript besides React (for demonstration)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
